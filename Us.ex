8254c6a2718d644129a89c634c4b6befe78c5a77
robotarmy <curtis@ram9.cc>
2014-11-07 10:19:34 -0800

    Universal Server

---
+++ Us.ex
@@ -0,0 +1,40 @@
+# This creates the Module Elixir.Us
+#
+defmodule Us do
+  def rtest(remote_pid,number) do
+    server_pid = spawn(&universal_server/0)
+    send(server_pid, {:become,&fac_server/0})
+    send(server_pid, {remote_pid, number})
+  end
+
+  def test(number) do
+    rtest(self(),number)
+    receive do
+       x -> x
+    end
+  end
+  def test() do
+    test(50).
+  end
+  defp universal_server do
+    receive do
+      {:become, f} -> f.()
+    end
+  end
+
+  defp fac_server do
+    receive do
+      {from,num} ->
+        send(from,factorial(num))
+        fac_server()
+    end
+  end
+
+  defp factorial(0) do
+    1
+  end
+
+  defp factorial(number) do
+    number * factorial(number-1)
+  end
+end
